0f54527c6f4adac9aed4675c45fe648d3b1d6e4c
JPivista <jpivistasolutions@gmail.com>
2023-12-06 11:05:10 +0530

    footer updated on invest impact

--- components/InvestForImpact/index.jsx
+++ components/InvestForImpact/index.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 
 import Header from '../SharedComponents/Header/'
+import Footer from '../SharedComponents/Footer'
+
 import InvestImpactTab from './InvestImpactTab'
 import InvestImpactBanner from './InvestImpactBanner'
 
 const index = () => {
   return (
     <>
-        <Header />
-        <InvestImpactBanner />
-          <InvestImpactTab />
+      <Header />
+      <InvestImpactBanner />
+      <InvestImpactTab />
+      <Footer />
     </>
   )
 }
