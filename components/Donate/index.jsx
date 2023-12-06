e86b58cc86ec063e8a7e111efc50c792ab1e0677
JPivista <jpivistasolutions@gmail.com>
2023-12-06 11:07:47 +0530

    donate page footer updated

--- components/Donate/index.jsx
+++ components/Donate/index.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 
 import Header from '../SharedComponents/Header'
+import Footer from '../SharedComponents/Footer'
+
 import DonateBanner from './DonateBanner'
 import DoanteTap from './DoanteTap'
 
 const index = () => {
   return (
     <>
-        <Header />
-            <DonateBanner />
-            <DoanteTap />
+      <Header />
+      <DonateBanner />
+      <DoanteTap />
+      <Footer />
     </>
   )
 }
